02b908801be96b9e6d6d215031ac269864748849
Cameron Shortt <cameron.b.shortt@gmail.com>
2024-02-16 17:20:29 -0600

    add some fun things

--- conf.el
+++ conf.el
@@ -20,50 +20,54 @@
   (find-file "~/.emacs"))
 
 (defun path-cons (dir)
-  (interactive "sDirectory to add? ")
-  (setenv "PATH" (concat dir (getenv "PATH")))
-  (setq exec-path (cons dir exec-path)))
+   (interactive "sDirectory to add? ")
+   (setenv "PATH" (concat dir (getenv "PATH")))
+   (setq exec-path (cons dir exec-path)))
 
 (use-package emacs
-  :bind (("C-=" . text-scale-increase)
-         ("C--" . text-scale-decrease)
-         ("C-c r" . reload)
-         ("C-c c" . open-conf)
-	 ("C-c i" . ibuffer)
-	 ("C-c s" . eshell)
-	 ("C-c w w" . delete-window)
-	 ("C-c w o" . delete-other-windows)
-	 ("C-c w s" . split-window-right)
-	 ("C-c w v" . split-window-below))
-  :hook (emacs-startup . (lambda () (message "(%s %d)"
+ :bind (("C-=" . text-scale-increase)
+        ("C--" . text-scale-decrease)
+        ("C-c r" . reload)
+        ("C-c c" . open-conf)
+	("C-c i" . ibuffer)
+	("C-c s" . eshell)
+	("C-c w w" . delete-window)
+	("C-c w o" . delete-other-windows)
+	("C-c w s" . split-window-right)
+	("C-c w v" . split-window-below))
+ :hook ((emacs-startup . (lambda () (message "(%s %d)"
 					     (format "%.2f"
 						     (float-time
 						      (time-subtract after-init-time before-init-time)))
-					     gcs-done)))
-  :config (setq truncate-lines t
-                inhibit-startup-echo-area-message t
-                inhibit-startup-message t
-		dired-use-ls-dired nil)
+					     gcs-done))))
+ :config (setq truncate-lines t
+               inhibit-startup-echo-area-message t
+               inhibit-startup-message t
+	       dired-use-ls-dired nil)
 
-	  (when (eq system-type 'darwin)
-	    (setenv "JAVA_HOME" "/opt/homebrew/opt/openjdk")
+ (when (eq system-type 'darwin)
+   (setenv "JAVA_HOME" "/opt/homebrew/opt/openjdk")
 
-	    (path-cons "/opt/homebrew/bin")
-	    (path-cons (concat (getenv "JAVA_HOME") "/bin"))
-	    (path-cons "/opt/local/bin"))
-          (path-cons (concat (getenv "HOME") "/bin"))
+   (message "Don't use macos D:<")
+
+   (path-cons "/opt/homebrew/bin")
+   (path-cons (concat (getenv "JAVA_HOME") "/bin"))
+   (path-cons "/opt/local/bin"))
+ (path-cons "/usr/local/bin")
+ (path-cons (concat (getenv "HOME") "/bin"))
 	  
-          (menu-bar-mode -1)
-          (tool-bar-mode -1)
-          (scroll-bar-mode -1)
+ (menu-bar-mode -1)
+ (tool-bar-mode -1)
+ (scroll-bar-mode -1)
 
-          (global-display-line-numbers-mode 1)
-          (global-visual-line-mode 1)
-          (column-number-mode)
+ (global-display-line-numbers-mode 1)
+ (global-visual-line-mode 1)
+ (column-number-mode)
 
-          (load-theme 'modus-vivendi)
+ (load-theme 'modus-vivendi)
 
-          (server-start))
+ (load "~/etc/fun.el")
+ (server-start))
 
 (use-package org
   :hook ((org-mode . org-indent-mode))
@@ -80,26 +84,34 @@
          (ielm-mode . enable-paredit-mode)
          (lisp-interaction-mode . enable-paredit-mode)
          (eval-expression-minibuffer-setup . enable-paredit-mode)
-         (org-mode . enable-paredit-mode)))
+	 (slime-repl-mode . enable-paredit-mode)))
+
+(use-package slime
+  :hook ((lisp-mode . slime))
+  :config (setq inferior-lisp-program "sbcl"))
 
 (use-package magit
   :bind ("C-c u" . magit-status))
 (use-package esup :config (setq esup-depth 0))
 
 (use-package god-mode
-  :bind ("<escape>" . god-mode-all)
+  :bind ("<tab> <tab>" . god-mode)
+  :hook ((emacs-startup . god-mode-all))
   :config (setq god-exempt-major-modes nil
 	        god-exempt-predicates nil ))
 
 (use-package eat
   :hook ((eshell-load . eat-eshell-mode)))
 
+(use-package teco
+  :bind ("<home>" . teco))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(god-mode esup magit paredit org-drill toc-org)))
+ '(package-selected-packages
+   '(slime teco sly toc-org paredit org-drill magit god-mode esup eat)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
